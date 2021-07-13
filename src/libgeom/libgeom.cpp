3f03926113616ba3e3837d34e15e8ae4b0281019
keshigomu2600 <keshigomu2600@docomo.ne.jp>
2021-07-13 13:43:54 +0900

    SIMD

--- src/libgeom/libgeom.cpp
+++ src/libgeom/libgeom.cpp
@@ -9,6 +9,18 @@ void copy_vector4_array(float* dst, const float* src, int num)
 {
 #if 1
 	// ToDo: SIMD計算を使って実装して下さい
+	float* pd = dst;
+	const float* ps = src;
+
+	for (int i = 0; i < num; i++) {
+		pd[0] = ps[0];
+		pd[1] = ps[1];
+		pd[2] = ps[2];
+		pd[3] = ps[3];
+
+		pd += 4;
+		ps += 4;
+	}
 #else
 	float* pd = dst;
 	const float* ps = src;
@@ -30,6 +42,20 @@ void add_vector4_array(float* dst, const float* src0, const float* src1, int num
 {
 #if 1
 	// ToDo: SIMD計算を使って実装して下さい
+	float* pd = dst;
+	const float* ps0 = src0;
+	const float* ps1 = src1;
+
+	for (int i = 0; i < num; i++) {
+		pd[0] = ps0[0] + ps1[0];
+		pd[1] = ps0[1] + ps1[1];
+		pd[2] = ps0[2] + ps1[2];
+		pd[3] = ps0[3] + ps1[3];
+
+		pd += 4;
+		ps0 += 4;
+		ps1 += 4;
+	}
 #else
 	float* pd = dst;
 	const float* ps0 = src0;
@@ -53,6 +79,18 @@ void apply_matrix_vector4_array(float* dst, const float* src, const float* matri
 {
 #if 1
 	// ToDo: SIMD計算を使って実装して下さい
+	float* pd = dst;
+	const float* ps = src;
+
+	for (int i = 0; i < num; i++) {
+		pd[0] = matrix[4 * 0 + 0] * ps[0] + matrix[4 * 0 + 1] * ps[1] + matrix[4 * 0 + 2] * ps[2] + matrix[4 * 0 + 3] * ps[3];
+		pd[1] = matrix[4 * 1 + 0] * ps[0] + matrix[4 * 1 + 1] * ps[1] + matrix[4 * 1 + 2] * ps[2] + matrix[4 * 1 + 3] * ps[3];
+		pd[2] = matrix[4 * 2 + 0] * ps[0] + matrix[4 * 2 + 1] * ps[1] + matrix[4 * 2 + 2] * ps[2] + matrix[4 * 2 + 3] * ps[3];
+		pd[3] = matrix[4 * 3 + 0] * ps[0] + matrix[4 * 3 + 1] * ps[1] + matrix[4 * 3 + 2] * ps[2] + matrix[4 * 3 + 3] * ps[3];
+
+		pd += 4;
+		ps += 4;
+	}
 #else
 	float* pd = dst;
 	const float* ps = src;
